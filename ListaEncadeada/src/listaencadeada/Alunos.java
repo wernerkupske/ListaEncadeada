11cfaec1c218b7cb27e651dca4c7cd08ffcf29c8
informatica <informatica@bauqi.blumenau.ifc.edu.br>
2017-09-15 10:05:02 -0300

    Fazer as operações do moodle

--- ListaEncadeada/src/listaencadeada/Alunos.java
+++ ListaEncadeada/src/listaencadeada/Alunos.java
@@ -9,13 +9,6 @@ package listaencadeada;
  *
  * @author informatica
  */
-public class ListaEncadeada {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
-    
+public class Alunos {
+    String nome;
 }
